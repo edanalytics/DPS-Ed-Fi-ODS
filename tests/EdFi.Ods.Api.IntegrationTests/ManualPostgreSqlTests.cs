cb3193f3187612cebeeac6640ce00e025f05cee7
travis.meador <t.meador@wearedoubleline.com>
2021-07-30 16:53:12 -0500

    communityProvider postgres wip

--- tests/EdFi.Ods.Api.IntegrationTests/ManualPostgreSqlTests.cs
+++ tests/EdFi.Ods.Api.IntegrationTests/ManualPostgreSqlTests.cs
@@ -40,5 +40,14 @@ namespace EdFi.Ods.Api.IntegrationTests
                 return BuildPostgreSqlConnection();
             }
         }
+
+        [TestFixture, Explicit]
+        public class PostgreSqlCommunityProviderTests : CommunityProviderTests
+        {
+            protected override IDbConnection BuildTestConnection()
+            {
+                return BuildPostgreSqlConnection();
+            }
+        }
     }
 }
